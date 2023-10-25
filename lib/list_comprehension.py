f079908d72dacdc9d3c5316c2d759ee5e56852b2
Kanny <kanny.ghafour87@gmail.com>
2023-10-25 18:06:48 -0500

    lab completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num = []
+    for n in num_list:
+        if n % 2 == 0:
+            even_num.append(n)
+    return even_num
+
 
 def make_exclamation(sentence_list):
-    pass
+
+    strings = []
+    for n in sentence_list:
+        strings.append(f"{n}!") 
+    return strings
+
+    
+ 
+
+
+
